0f59f369c6397f7962b24a77137badd4d9e723f4
Bjarne Riis <bjarne@briis.com>
2021-01-08 21:45:54 +0100

    Fixed changes in Forecast API

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pysmartweatherio',
   packages = ['pysmartweatherio'],
-  version = '2.1.4',
+  version = '2.1.5',
   license='MIT',
   description = 'Python Wrapper for Smart Home Weather REST API', 
   long_description=" ".join(
